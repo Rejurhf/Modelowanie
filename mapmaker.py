102e2e801663fbc115cfe4fff5fc86cb9e73c656
Rejurhf <mziomek108@gmail.com>
2019-01-09 23:10:34 +0100

    map maker updated

--- mapmaker.py
+++ mapmaker.py
@@ -3,16 +3,26 @@
 
 from PIL import Image
 from filecontroller import saveArrayToJSON
+import numpy as np
 
 size = 256, 256
 try:
-    im = Image.open("test.jpg") # read image
+    im = Image.open("zatoka.png") # read image
     im = im.resize(size, Image.ANTIALIAS)   # resize image to size
     gray = im.convert('L')  # conversion to gray scale
-    bw = gray.point(lambda x: 0 if x<128 else 255, '1')  # binarization 0 and 255
+    bw = gray.point(lambda x: 0 if x<209 else 255, '1')  # binarization 0 and 255
     bw.save("test_bw.png") # save it
 
-    bw_bin = gray.point(lambda x: 0 if x<128 else 1, '1') # binarization 0 and 1
-    saveArrayToJSON("maps.json", "europe", list(bw_bin.getdata())) # save to json
+    bw = bw.rotate(90)
+    array = np.asarray(bw)
+    tmp = np.zeros(size)
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+                if array[j,i]:
+                    tmp[254-i,254-j] = 0
+                else:
+                    tmp[254-i,254-j] = -1
+
+    saveArrayToJSON("maps.json", "zatoka", tmp) # save to json
 except IOError:
     print("cannot create image")
