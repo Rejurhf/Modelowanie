31da2262c5eb2dd2452e70293ad9f66f1219b0d4
Rejurhf <mziomek108@gmail.com>
2019-01-10 13:09:53 +0100

    current maker works with many currents

--- Symulacje_Python/current.py
+++ Symulacje_Python/current.py
@@ -11,7 +11,7 @@ current = np.empty((rowmax,colmax),object)    # array with current
 array = np.zeros((rowmax,colmax)) # init array with shema of current
 
 try:
-    im = Image.open("img8x8.png")   # read image
+    im = Image.open("img8x8t2.png")   # read image
     gray = im.convert('L')  # conversion to gray scale
     array = np.asarray(gray)    # conversion to array
 except IOError:
@@ -19,45 +19,54 @@ except IOError:
 
 array = np.where(array == 0, 254, array)    # repleacing all 0 with 254
 array = np.where(array == 255, 0, array)
-red = list(zip(*np.where(array == 91)))    # geting tuple of red dot
-green = list(zip(*np.where(array == 122)))   # geting tuple of green dot
+redlist = list(zip(*np.where(array == 91)))    # geting tuple of red dot
+greenlist = list(zip(*np.where(array == 122)))   # geting tuple of green dot
 
 #---------------------------------------------------------------------------
-# manage red pixel
-if green[0]-red[0] != 0 and green[1]-red[1] != 0:   # check if green in the angle
-    current[red[0],red[1]] = \
-        (round(((green[0]-red[0])/(2**(1/2))),2), \
-        round(((green[1]-red[1])/(2**(1/2))),2))
-else:
-    current[red[0],red[1]] = (float(green[0]-red[0]), float(green[1]-red[1]))
-
-#------------------------------------------------------------------------------
-# manage green pixel
-# if pixel in the edge of array
-if green[0] == rowmax - 1 or green[1] == colmax - 1 or green[0] == 0 or green[1] == 0:
-    current[green[0], green[1]] = current[red[0],red[1]]
-else:
-    rowtoadd = 0    # sum of neighbours positions
-    coltoadd = 0
-    for r in range(-1,2):   # get informations about neightboars
-        for c in range(-1,2):
-            if array[green[0]+r,green[1]+c] == 254:
-                rowtoadd += r
-                coltoadd += c
-                tovisitqueue.append((green[0]+r,green[1]+c))
-
-    if rowtoadd == 0 and coltoadd == 0:
-        current[green[0], green[1]] = current[red[0],red[1]]
-    elif rowtoadd == 0 or coltoadd == 0:
-        if rowtoadd == 0:
-            current[green[0], green[1]] = (0., coltoadd/np.abs(coltoadd))
-        else:
-            current[green[0], green[1]] = (rowtoadd/np.abs(rowtoadd), 0.)
+# manage red and grin pixel
+for red, green in zip(redlist, greenlist):   # comment this line if got only one current
+    print(red[0], red[1])
+    print(green[0], green[1])
+    print("----------------")
+    if green[0]-red[0] != 0 and green[1]-red[1] != 0:   # check if green in the angle
+        current[red[0],red[1]] = \
+            (round(((green[0]-red[0])/(2**(1/2))),2), \
+            round(((green[1]-red[1])/(2**(1/2))),2))
     else:
-        current[green[0], green[1]] = \
-            (round(((rowtoadd/(rowtoadd+coltoadd))/(2**(1/2))),2), \
-            round(((coltoadd/(rowtoadd+coltoadd))/(2**(1/2))),2))
+        current[red[0],red[1]] = (float(green[0]-red[0]), float(green[1]-red[1]))
+    #---------------------------------------------------------------------------
+    # manage green pixel
+    # if pixel in the edge of array
+    if green[0] == rowmax - 1 or green[1] == colmax - 1 or green[0] == 0 or green[1] == 0:
+        current[green[0], green[1]] = current[red[0],red[1]]
+        for r in range(-1,2):   # get informations about neightboars
+            for c in range(-1,2):
+                if green[0]+r >= 0 and green[1]+c >= 0 and green[0]+r < rowmax and \
+                        green[1]+c < colmax and (r != 0 and c != 0):
+                    tovisitqueue.append((green[0]+r,green[1]+c))
+    else:
+        rowtoadd = 0    # sum of neighbours positions
+        coltoadd = 0
+        for r in range(-1,2):   # get informations about neightboars
+            for c in range(-1,2):
+                if array[green[0]+r,green[1]+c] == 254:
+                    rowtoadd += r
+                    coltoadd += c
+                    tovisitqueue.append((green[0]+r,green[1]+c))
 
+        if rowtoadd == 0 and coltoadd == 0:
+            current[green[0], green[1]] = current[red[0],red[1]]
+        elif rowtoadd == 0 or coltoadd == 0:
+            if rowtoadd == 0:
+                current[green[0], green[1]] = (0., coltoadd/np.abs(coltoadd))
+            else:
+                current[green[0], green[1]] = (rowtoadd/np.abs(rowtoadd), 0.)
+        else:
+            current[green[0], green[1]] = \
+                (round(((rowtoadd/(rowtoadd+coltoadd))/(2**(1/2))),2), \
+                round(((coltoadd/(rowtoadd+coltoadd))/(2**(1/2))),2))
+
+print(tovisitqueue)
 #--------------------------------------------------------------------------
 # manage pixels == 254
 while len(tovisitqueue) > 0:
